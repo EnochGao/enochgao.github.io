b6c012e044c3d256a11daaf2e0780ea887202b8e
enochgao <enochgao@qq.com>
2023-12-07 13:20:51 +0800

    chore: chore

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,49 +1,49 @@
-import { defaultTheme, defineUserConfig } from 'vuepress';
-import { backToTopPlugin } from '@vuepress/plugin-back-to-top';
-import { sidebar } from './configs/sidebar';
-import { navbar } from './configs/navbar';
-import { getDirname, path } from '@vuepress/utils';
+import { defaultTheme, defineUserConfig } from "vuepress";
+import { backToTopPlugin } from "@vuepress/plugin-back-to-top";
+import { sidebar } from "./configs/sidebar";
+import { navbar } from "./configs/navbar";
+import { getDirname, path } from "@vuepress/utils";
 
 const __dirname = getDirname(import.meta.url);
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 export default defineUserConfig({
-  lang: 'zh-CN',
-  title: 'EnochGao的博客',
-  description: '专注于Angular开发',
-  base: '/blog/',
+  lang: "zh-CN",
+  title: "EnochGao的博客",
+  description: "一名前端开发者，技术并不是生活的全部",
+  base: "/blog/",
   theme: defaultTheme({
-    logo: 'https://avatars.githubusercontent.com/u/41459067?v=4',
-    repo: 'https://github.com/EnochGao/blog',
-    docsDir: 'docs',
+    logo: "https://avatars.githubusercontent.com/u/41459067?v=4",
+    repo: "https://github.com/EnochGao/blog",
+    docsDir: "docs",
     locales: {
-      '/': {
+      "/": {
         navbar,
         sidebar,
-        selectLanguageName: '简体中文',
-        selectLanguageText: '选择语言',
-        selectLanguageAriaLabel: '选择语言',
+        selectLanguageName: "简体中文",
+        selectLanguageText: "选择语言",
+        selectLanguageAriaLabel: "选择语言",
         // page meta
-        editLinkText: '在 GitHub 上编辑此页',
-        lastUpdatedText: '上次更新',
-        contributorsText: '贡献者',
+        editLinkText: "在 GitHub 上编辑此页",
+        lastUpdatedText: "上次更新",
+        contributorsText: "贡献者",
         // custom containers
-        tip: '提示',
-        warning: '注意',
-        danger: '警告',
+        tip: "提示",
+        warning: "注意",
+        danger: "警告",
         // 404 page
         notFound: [
-          '这里什么都没有',
-          '我们怎么到这来了？',
-          '这是一个 404 页面',
-          '看起来我们进入了错误的链接',
+          "这里什么都没有",
+          "我们怎么到这来了？",
+          "这是一个 404 页面",
+          "看起来我们进入了错误的链接",
         ],
-        backToHome: '返回首页',
+        backToHome: "返回首页",
         // a11y
-        openInNewWindow: '在新窗口打开',
-        toggleColorMode: '切换颜色模式',
-        toggleSidebar: '切换侧边栏',
+        openInNewWindow: "在新窗口打开",
+        toggleColorMode: "切换颜色模式",
+        toggleSidebar: "切换侧边栏",
       },
     },
     themePlugins: {
@@ -53,8 +53,6 @@ export default defineUserConfig({
       prismjs: !isProd,
     },
   }),
-
   plugins: [backToTopPlugin()],
-  alias: {
-  },
+  alias: {},
 });
