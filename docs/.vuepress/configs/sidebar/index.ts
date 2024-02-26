1f593443635f7711ecfd7f7752d901f7a8cb69ea
enochgao <enochgao@qq.com>
2024-02-26 10:18:17 +0800

    feat: 指差确认法

--- docs/.vuepress/configs/sidebar/index.ts
+++ docs/.vuepress/configs/sidebar/index.ts
@@ -4,7 +4,7 @@ export const sidebar: SidebarConfig = {
   '/chore': [
     {
       text: '',
-      children: ['/chore/chore.md', '/chore/emoji.md', '/chore/vscode.md','/chore/git.md'],
+      children: ['/chore/chore.md', '/chore/emoji.md', '/chore/vscode.md','/chore/git.md','/chore/pointing-and-calling'],
     },
   ],
   '/angular/directive': [
