44eac8b103b98785a4baec7c8d132d08cf72143e
EnochGao <enochgao@qq.com>
2025-01-08 00:01:09 +0800

    chore: chore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
-import { BlogCardComponent } from './component/blog-card/blog-card.component';
 import { ProfileComponent } from './component/profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [BlogCardComponent, ProfileComponent],
+  imports: [ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
